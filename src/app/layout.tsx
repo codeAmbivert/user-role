dc59609a178dc4ad8fcb5003ac9e58c016618ca6
codeAmbivert <titilopechisom20@gmail.com>
2025-06-30 00:23:03 +0100

    aspect fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import "./index.css";
 import { Inter } from "next/font/google";
 import PageLayout from "@/components/layout/PageLayout";
 
